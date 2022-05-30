14841e5386b20edadaef147655cb0d2dc8f3be37
GonzoTheDev <c20703429@mytudublin.ie>
2022-05-30 20:54:12 +0100

    Install amplify auth dependencies

--- gigachat-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ gigachat-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,14 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "gigachatfrontenda0efcfd2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
